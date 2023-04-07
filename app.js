e6839a085a66e81f10fa3948550a72991a3ab733
Chinmaya Sahu <chinmayasahumac@gmail.com>
2023-04-08 00:00:28 +0530

    added css

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require("express")
 const https = require("https");
 const bodyParser = require("body-parser")
 const app = express();
+app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
